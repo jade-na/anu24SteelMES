82bd8f437c746a601d9900203197d35f60b360ae
myoungjin-na <scissorhandz@naver.com>
2024-11-17 18:43:07 +0900

    grpc 프로젝트 적용 연습. grpcDummyMesServer 프로젝트 업로드

---
+++ src/prototypes/grpcDummyMesServer/grpcDummyMesServer/DBServiceServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using SteelMES;
+
+namespace grpcDummyMesServer
+{
+    public class DBServiceServer : DB_Service.DB_ServiceBase
+    {
+        public override Task<prodHistoryInfoReply> reqProdHistory(prodHistoryInfoRequest request, ServerCallContext context)
+        {
+            // request.FromTime;
+            // request.ToTime;
+            var result = new prodHistoryInfoReply();
+
+            result.ErrorCode = 0;
+
+            var info1 = new prodHistoryInfo();
+            info1.ProdName = "A";
+            info1.SerialNum = "0001";
+            info1.LotId = "LOT A";
+            info1.DefactCode = "DEFACT A";
+            info1.ProduceDay = "2024-11-17 00:00:00";
+            result.Infos.Add(info1);
+
+            var info2 = new prodHistoryInfo();
+            info2.ProdName = "B";
+            info2.SerialNum = "0002";
+            info2.LotId = "LOT A";
+            info2.DefactCode = "DEFACT B";
+            info2.ProduceDay = "2024-11-17 00:00:10";
+            result.Infos.Add(info2);
+
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ src/prototypes/grpcDummyMesServer/grpcDummyMesServer/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace grpcDummyMesServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 71);
+            button1.Name = "button1";
+            button1.Size = new Size(115, 62);
+            button1.TabIndex = 0;
+            button1.Text = "start";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(56, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(127, 23);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "50051";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Port : ";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(168, 71);
+            button2.Name = "button2";
+            button2.Size = new Size(115, 62);
+            button2.TabIndex = 3;
+            button2.Text = "stop";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(323, 166);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button2;
+    }
+}
